947d556d6cf8e3629530cf09ef126b0284014091
lavesh4807 <142747980+lavesh4807@users.noreply.github.com>
2023-09-23 16:52:11 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div className='navbar-container'>
+      <div className='navbar-brand'>Dev@Deakin</div>
+      <input className='search-input' type='text' placeholder='Search' />
+      <button className='navbar-button'>Post</button>
+      <Link to='/login'>
+        <button className='login-button'>Login</button>
+      </Link>
+      <Link to='/signup'>
+        <button className='signup-button'>Sign-Up</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ Routing.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './Navbar'; 
+import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
+import { auth } from './FireBase';
+// import Signout from './Signout';
+
+function Routing() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default Routing;
